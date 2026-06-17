2b55d76ca5d4f71149a5da3835a2b1a23cb04dfc
Melivor4Capensis <borsukmiodozerny@gmail.com>
2026-06-17 15:54:14 +0200

    Delete Projekt directory

--- Projekt/JS/dayTypeSelectStyleChange.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.day-type-select').forEach(sel => {
-    sel.classList.toggle('roboczy', sel.value === 'Roboczy');
-
-    sel.addEventListener('change', function () {
-        this.classList.toggle('roboczy', this.value === 'Roboczy');
-    });
-});
--- Projekt/JS/reportStatusSelectStyleChange.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.report-status-select').forEach(sel => {
-    sel.classList.toggle('zatwierdzony', sel.value === 'Zatwierdzony');
-
-    sel.addEventListener('change', function () {
-        this.classList.toggle('zatwierdzony', this.value === 'Zatwierdzony');
-    });
-});
